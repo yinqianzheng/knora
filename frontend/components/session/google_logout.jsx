927cfbba48e398611e8aa6fe7f03a54605651f15
Yinqian Zheng <yinqianzheng@gmail.com>
2020-01-08 02:10:18 -0800

    update google signin client_id for heroku

--- frontend/components/session/google_logout.jsx
+++ frontend/components/session/google_logout.jsx
@@ -11,20 +11,6 @@ export default class GoogleLogout extends React.Component {
     if (auth2 != null) {
       auth2.signOut().then(auth2.disconnect().then(this.props.onLogoutSuccess));
     }
-    // gapi.load("client:auth2", () => {
-    //   gapi.client
-    //     .init({
-    //       clientId:
-    //         "410414924194-p01fmqs2gn56sgg4af6alb20mu3hojs3.apps.googleusercontent.com",
-    //       scope: "profile email"
-    //     })
-    //     .then(() => {
-    //       const auth = gapi.auth2.getAuthInstance();
-    //       auth
-    //         .signOut()
-    //         .then(auth.disconnect().then(this.props.onLogoutSuccess));
-    //     });
-    // });
   }
 
   render() {
