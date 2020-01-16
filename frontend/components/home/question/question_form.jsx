27f2f486647fbc5f3fec21ef073caa48232efbb3
Yinqian Zheng <yinqianzheng@gmail.com>
2020-01-16 12:35:21 -0800

    add css for add question modal

--- frontend/components/home/question/question_form.jsx
+++ frontend/components/home/question/question_form.jsx
@@ -13,6 +13,7 @@ export default class QuestionForm extends React.Component {
     this.preventClickThrough = this.preventClickThrough.bind(this);
     this.updateQustion = this.updateQustion.bind(this);
     this.submit = this.submit.bind(this);
+    this.renderAskAdvice = this.renderAskAdvice.bind(this);
   }
 
   hideModal() {
@@ -65,6 +66,85 @@ export default class QuestionForm extends React.Component {
     }
   }
 
+  renderAskAdvice() {
+    return (
+      <div className="add-question-info">
+        <h3>Tips on getting good answers quickly</h3>
+        <ul>
+          <li>
+            <svg
+              className="check-icon"
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              version="1.1"
+            >
+              <g
+                fill="none"
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M12,3 C16.971,3 21,7.029 21,12 C21,16.971 16.971,21 12,21 C7.029,21 3,16.971 3,12 C3,7.029 7.029,3 12,3 Z M8,12.6290909 L10.9090909,15.5381818 L16,9"
+                  stroke="#329bff"
+                  stroke-width="1.5"
+                ></path>
+              </g>
+            </svg>
+            Make sure your question hasn't been asked already
+          </li>
+          <li>
+            <svg
+              className="check-icon"
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              version="1.1"
+            >
+              <g
+                fill="none"
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M12,3 C16.971,3 21,7.029 21,12 C21,16.971 16.971,21 12,21 C7.029,21 3,16.971 3,12 C3,7.029 7.029,3 12,3 Z M8,12.6290909 L10.9090909,15.5381818 L16,9"
+                  stroke="#329bff"
+                  stroke-width="1.5"
+                ></path>
+              </g>
+            </svg>
+            Keep your question short and to the point
+          </li>
+          <li>
+            <svg
+              className="check-icon"
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              version="1.1"
+            >
+              <g
+                fill="none"
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M12,3 C16.971,3 21,7.029 21,12 C21,16.971 16.971,21 12,21 C7.029,21 3,16.971 3,12 C3,7.029 7.029,3 12,3 Z M8,12.6290909 L10.9090909,15.5381818 L16,9"
+                  stroke="#329bff"
+                  stroke-width="1.5"
+                ></path>
+              </g>
+            </svg>
+            Double-check grammar and spelling
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   preventClickThrough(e) {
     e.stopPropagation();
   }
@@ -98,17 +178,22 @@ export default class QuestionForm extends React.Component {
               onClick={e => this.preventClickThrough(e)}
               className="question-form-container"
             >
-              <div>
-                <input
-                  className="question-form-input"
-                  onChange={e => this.updateQustion(e)}
-                  type="text"
-                  name="title"
-                  value={this.state.title}
-                />
-                <div className="question-form-footer">
-                  <button onClick={this.submit}>{this.props.formType}</button>
+              <div className="question-form-header">
+                <div>
+                  {this.props.type === "EDIT" ? "Edit" : "Add Question"}
                 </div>
+              </div>
+              {this.renderAskAdvice()}
+              <textarea
+                className="question-form-input"
+                onChange={e => this.updateQustion(e)}
+                type="text"
+                name="title"
+                placeholder={`Start your question with \"What\", \"How\", \"Why\", etc`}
+                value={this.state.title}
+              ></textarea>
+              <div className="question-form-footer">
+                <button onClick={this.submit}>{this.props.formType}</button>
               </div>
             </div>
           </div>
